da02ba20e71d6f16985518700fc58945d9645705
hotan4866 <hotan4866@gmail.com>
2024-08-28 01:22:32 +0900

    그리드 쉐이더

--- Assets/_Scripts/PlacementSystem.cs
+++ Assets/_Scripts/PlacementSystem.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
-    [SerializeField] GameObject mouseIndicator;
+    [SerializeField] GameObject mouseIndicator, cellIndicator;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] Grid grid;
 
     private void Update()
     {
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 
 }
